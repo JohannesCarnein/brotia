b89d5533d3ac427b5c55397eaab0fb1c4d43a2e5
JohannesCarnein <118236397+JohannesCarnein@users.noreply.github.com>
2025-11-02 22:46:47 +0100

    Update and rename DQL.sql to DB_Carnein_1c.sqlmoin
    Moin

--- DB_Carnein_1c.sql
+++ DB_Carnein_1c.sql
@@ -45,3 +45,39 @@ select strasse, plz, stadt, anzahl_raeume, anzahl_betten, kueche, raucher, sauna
 	from fewo
 	left join saisonale_preise as sp
 	on fewo.id = sp.fewo_id and @wunschdatum between sp.saison_start and sp.saison_ende;
+
+DML update und delete
+
+-- update der beschreibung für eine wohnung über die attribute, die als unique key genutzt werden
+update fewo
+	set beschreibung = 'Schöne Wohnung mit Blick aufs Wasser'
+	where strasse = 'Hafenweg 7'
+		and plz = '23673'
+		and stadt = 'Schönkirchen';
+
+-- Wohnungen mit einer durchschnittlichen Bewertung von weniger als 3 Sternen bekommen den Bannhammer zu spüren
+SET SQL_SAFE_UPDATES = 0;
+delete from fewo
+	where id in (
+		select fewo.id from ( -- schnappt sich alle ids aus der Menge in Klammern
+			select fewo.id, avg(bewertung.sterne) as durchschnitt_bewertung
+				from fewo
+				inner join buchung on buchung.fewo_id = fewo.id
+				inner join bewertung on bewertung.buchung_id = buchung.id
+				group by fewo.id -- erzeugt eine Schnittmenge von fewos mit Buchungen und Bewertungen, die durch den fewo index gruppiert sind
+				having avg(bewertung.sterne) < 3 -- reduziert die Menge auf fewos, die weniger als 3 Sterne haben
+		) as temp -- wir brauchen einen alias damit die subquery unterschieden werden kann
+	);
+SET SQL_SAFE_UPDATES = 1;
+    
+-- DDL alter table statements
+select version();
+
+alter table fewo 
+	add column if not exists titel varchar(50);
+
+alter table fewo 
+	rename column titel to bezeichnung;
+
+alter table fewo 
+	drop column bezeichnung;
